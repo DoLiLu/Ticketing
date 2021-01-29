2c0190692901327965a0f407c24d712df8f8daf6
DESKTOP-T6O4081\lee <lsecret@naver.com>
2021-01-29 18:01:48 +0900

    modify

---
+++ src/main/java/com/project/ticketing/common/type/Gender.java
@@ -0,0 +1,5 @@
+package com.project.ticketing.common.type;
+
+public enum Gender {
+    MALE, FEMALE
+}
--- src/main/java/com/project/ticketing/user/entity/UserEntity.java
+++ src/main/java/com/project/ticketing/user/entity/UserEntity.java
@@ -1,14 +1,25 @@
 package com.project.ticketing.user.entity;
 
+import com.project.ticketing.common.type.Gender;
+import lombok.Builder;
+import lombok.RequiredArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
 @Entity
+@RequiredArgsConstructor
+@Builder
 public class UserEntity {
     @Id
     String userKey;
 
     String userId;
 
+    String email;
+
+    String birthday;
+
+    Gender gender;
 
 }
